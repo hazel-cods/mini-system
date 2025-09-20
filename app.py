4f8eedb713f92bc312938ca72d1ab57b684f485d
hazelanncreator <solanohazel123@gmail.com>
2025-09-20 21:30:15 +0800

    delete

--- app.py
+++ app.py
@@ -5,20 +5,6 @@ app = Flask(__name__)
 
   
 
-#database connection
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Hazel123%401009@localhost/mini_system'
-app.config['SQLALCHEMY_TRACT_MODIFICATION'] = False
-
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(80), nullable=False)
-  email = db.Column(db.String(100),nullable=False, unique=True)
-
-  
-
 
 if __name__ == "__main__":
   app.run(debug=True)
