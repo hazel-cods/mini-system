6f3daf46e1ba15af79914508daffce840883f0f3
hazelanncreator <solanohazel123@gmail.com>
2025-09-20 21:33:09 +0800

    add routes

--- app.py
+++ app.py
@@ -3,7 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-  
+
+@app.route("/")
+def index():
+  return render_template(index.html)
+@app.route("/message")
+def message():
+  return render_template("message.html")
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
